a64151b93672e5e8b59a0e87c1e461455b2f2d5d
Tony LaForgia <alforgia428@gmail.com>
2022-02-11 15:08:31 -0600

    HTML created. JS, jQ, CSS all linked. SERVER is up an running. POOL and Router functional.

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -11,16 +11,14 @@ let poolOptions = {
   idleTimeoutMillis: 30_000,
 };
 
-// create a pool instance for the koala database
 const toDoPool = new pg.Pool(poolOptions);
 
-// give an indicator when connected to database
 toDoPool.on("connect", () => {
   console.log("Connected to weekend-to-do-app Database");
 });
 
 // handle database errors
-koalaPool.on("error", (err) => {
+toDoPool.on("error", (err) => {
   console.error("Error with weekend-to-do-app Database:", err);
 });
 
