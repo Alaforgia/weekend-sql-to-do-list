4500a2a3b1ab303400b2bae23d523826ac2bb688
Tony LaForgia <alforgia428@gmail.com>
2022-02-11 15:58:04 -0600

    Server side GET returns Database in Postman

--- server/routes/todo-router.js
+++ server/routes/todo-router.js
@@ -5,5 +5,18 @@ const toDoRouter = express.Router();
 const pool = require("../modules/pool");
 
 // GET
+toDoRouter.get("/", (req, res) => {
+  let queryText = 'SELECT * FROM "task-list"';
+  console.log("GETTING?!");
+  pool
+    .query(queryText)
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((error) => {
+      console.log("GET ERROR", error);
+      res.sendStatus(500);
+    });
+});
 
 module.exports = toDoRouter;
