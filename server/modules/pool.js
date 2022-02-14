950b693e77c91d6f03c7d05361046951f813e1d1
Tony LaForgia <alforgia428@gmail.com>
2022-02-14 16:14:02 -0600

    Update for heroku

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,24 +1,78 @@
-const pg = require("pg");
-// database pool options
-let poolOptions = {
-  database: process.env.DATABASE_NAME || "weekend-to-do-app", // CHANGE THIS LINE to match you local database name!
-  host: "localhost",
-  max: 10,
-  // database on port 5432
-  port: 5432,
-  // timeout after 30 seconds of no connection
-  idleTimeoutMillis: 30_000,
-};
+// const pg = require("pg");
+// // database pool options
+// let poolOptions = {
+//   database: process.env.DATABASE_NAME || "weekend-to-do-app", // CHANGE THIS LINE to match you local database name!
+//   host: "localhost",
+//   max: 10,
+//   // database on port 5432
+//   port: 5432,
+//   // timeout after 30 seconds of no connection
+//   idleTimeoutMillis: 30_000,
+// };
 
-const toDoPool = new pg.Pool(poolOptions);
+// const toDoPool = new pg.Pool(poolOptions);
 
-toDoPool.on("connect", () => {
-  console.log("Connected to weekend-to-do-app Database");
+// toDoPool.on("connect", () => {
+//   console.log("Connected to weekend-to-do-app Database");
+// });
+
+// // handle database errors
+// toDoPool.on("error", (err) => {
+//   console.error("Error with weekend-to-do-app Database:", err);
+// });
+
+// module.exports = toDoPool;
+
+
+/**
+* You'll need to use environment variables in order to deploy your
+* pg-pool configuration to Heroku.
+* It will look something like this:
+**/
+/* the only line you likely need to change is
+ database: 'prime_app',
+ change `prime_app` to the name of your database, and you should be all set!
+*/
+​
+const pg = require('pg');
+const url = require('url');
+​
+let config = {};
+​
+// We need a different pg configuration if we're running
+// on Heroku, vs if we're running locally.
+//
+// Heroku gives us a process.env.DATABASE_URL variable,
+// so if that's set, we know we're on heroku.
+if (process.env.DATABASE_URL) {
+  config = {
+    // We use the DATABASE_URL from Heroku to connect to our DB
+    connectionString: process.env.DATABASE_URL,
+    // Heroku also requires this special `ssl` config
+    ssl: { rejectUnauthorized: false },
+  };
+} else {
+  // If we're not on heroku, configure PG to use our local database
+  config = {
+    host: 'localhost',
+    port: 5432,
+    database: process.env.DATABASE_NAME || 'weekend-to-do-app', // CHANGE THIS LINE to match your local database name!
+  };
+}
+​
+// this creates the pool that will be shared by all other modules
+const pool = new pg.Pool(config);
+​
+// the pool will log when it connects to the database
+pool.on('connect', () => {
+  console.log('Postgesql connected');
 });
-
-// handle database errors
-toDoPool.on("error", (err) => {
-  console.error("Error with weekend-to-do-app Database:", err);
+​
+// the pool with emit an error on behalf of any idle clients
+// it contains if a backend error or network partition happens
+pool.on('error', (err) => {
+  console.log('Unexpected error on idle client', err);
+  process.exit(-1);
 });
-
-module.exports = toDoPool;
+​
+module.exports = toDopool;
